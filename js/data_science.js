fbd799e94352355164bc4e3925abcae3980b6030
Edward <edabsw@gmail.com>
2014-03-12 16:50:56 -0700

    Code editor + UI tweaks.

--- js/data_science.js
+++ js/data_science.js
@@ -4,25 +4,39 @@
   var App = root.App = (root.App || {});
 
   var controller = App.controller = function (description, data, initialCode) {
-    this.description = description
+    this.description = description;
     this.data = data;
     this.initialCode = initialCode;
-    this.editor = $('#edit-area');
-    this.results = $('#results');
-    $('#submit').click(this.submit.bind(this));
+    this.output = $('#output');
+    $('#submit').click(this.run.bind(this));
+    this.setupEditor();
     this.displayInfo();
+    this.run();
   }
 
   controller.prototype = {
     displayInfo: function () {
       $('#desc').append(this.description);
       $('#data').text(JSON.stringify(this.data));
-      this.editor.html(this.initialCode);
+      this.editor.setValue(this.initialCode);
     },
 
-    submit: function () {
+    run: function () {
       var data = this.data;
-      this.results.text(eval(this.editor[0].value));
+      this.output.text(eval(this.editor.getValue()));
+    },
+
+    setupEditor: function () {
+      var editor = this.editor = ace.edit("editor");
+        editor.setTheme("ace/theme/monokai");
+      var session = editor.getSession();
+        session.setMode("ace/mode/javascript");
+        session.setTabSize(2);
+        session.setUseSoftTabs(true);
+      editor.commands.addCommand({
+        bindKey: {win: 'Ctrl-Enter', mac: 'Command-Enter'},
+        exec: this.run.bind(this)
+      });
     }
   }
 })(this);
@@ -36,6 +50,7 @@ $(function () {
   var controller = new App.controller(
     "<br>The current data is about a set of numbers.<br>Please find the average of the set.",
     data,
-    "var sum = data.reduce(function (sum, el) { \n  return sum + el; \n}); \n\n'Average: ' + sum / data.length;"
+    "data.join('\\n');"
   );
+
 });
