0e6da50e4d260e562f985316a2c2b4c21965724b
Edward <edabsw@gmail.com>
2014-03-12 11:20:33 -0700

    Initial app setup / structure

---
+++ js/data_science.js
@@ -0,0 +1,38 @@
+'use strict';
+
+(function (root) {
+  var App = root.App = (root.App || {});
+
+  var controller = App.controller = function (description, data) {
+    this.description = description
+    this.data = data;
+    this.editor = $('#edit-area');
+    this.results = $('#results');
+    $('#submit').click(this.submit.bind(this));
+    this.displayInfo();
+  }
+
+  controller.prototype = {
+    displayInfo: function () {
+      $('#desc').append(this.description);
+      $('#data').text(JSON.stringify(this.data));
+    },
+
+    submit: function () {
+      var data = this.data;
+      this.results.text(eval(this.editor.text()));
+    }
+  }
+})(this);
+
+$(function () {
+  var data = [];
+  for(var i = 0; i < 500; i++) {
+    data.push(i);
+  }
+
+  var controller = new App.controller(
+    "<br>The current data is about numbers.<br>Please find the average.",
+    data
+  );
+});
